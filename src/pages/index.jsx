fe89b1dc8e5559d8864d8dd79c8d0121a746f99d
Francoise <84253938+FrancoiseC@users.noreply.github.com>
2024-03-05 01:08:39 +0000

    Traffic lights

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "react-bootstrap";
 import { Jumbotron } from "@/components/Jumbotron";
+import { TrafficLight } from "@/components/TrafficLight";
 
 export default function Home() {
   return (
     <Container>
       <Jumbotron />
+      <TrafficLight/>
+    
+
     </Container>
   );
 }
